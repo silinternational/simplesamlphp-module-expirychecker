920425cd84e50dc322f48b4c662f752b27b45d53
Matt Henderson <matt_henderson@sil.org>
2017-06-07 13:33:51 -0400

    Set more necessary config options in enable-debug.sh.

--- development/enable-debug.sh
+++ development/enable-debug.sh
@@ -9,6 +9,10 @@ INI_FILE="/etc/php/7.0/apache2/php.ini"
 echo "Configuring debugger in $INI_FILE"
 echo "xdebug.remote_enable=1" >> $INI_FILE
 echo "xdebug.remote_host=$XDEBUG_REMOTE_HOST" >> $INI_FILE
+echo "xdebug.remote_autostart=1" >> $INI_FILE
+echo "xdebug.remote_connect_back=1" >> $INI_FILE
+echo "xdebug.remote_port=9000" >> $INI_FILE
+echo "xdebug.idekey=netbeans-xdebug" >> $INI_FILE
 
 mkdir -p /data/vendor/simplesamlphp/simplesamlphp/modules/sildisco
 touch /data/vendor/simplesamlphp/simplesamlphp/modules/sildisco/default-enable
