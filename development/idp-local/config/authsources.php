c332bc167ffd2d25f73bef982f529c43eeb6a9f3
Matt Henderson <matt_henderson@sil.org>
2017-06-08 16:10:43 -0400

    Improve dummy user's names (avoiding keywords I may look for in other content, and to add clarity).

--- development/idp-local/config/authsources.php
+++ development/idp-local/config/authsources.php
@@ -3,38 +3,38 @@
 $config = [
     'example-userpass' => [
         'exampleauth:UserPass',
-        'normal:a' => [
-            'eduPersonPrincipalName' => ['NORMAL-hub-idp.local'],
+        'distant_future:a' => [
+            'eduPersonPrincipalName' => ['DISTANT_FUTURE-hub-idp.local'],
             'eduPersonTargetID' => ['11111111-1111-1111-1111-111111111111'],
-            'sn' => ['User'],
-            'givenName' => ['Normal'],
-            'mail' => ['normal@example.com'],
+            'sn' => ['Future'],
+            'givenName' => ['Distant'],
+            'mail' => ['distant_future@example.com'],
             'employeeNumber' => ['11111'],
-            'cn' => ['NORMAL'],
+            'cn' => ['DISTANT_FUTURE'],
             'schacExpiryDate' => [
                 gmdate('YmdHis\Z', strtotime('+6 months')), // Distant future
             ],
         ],
-        'warn:b' => [
-            'eduPersonPrincipalName' => ['WARN@ssp-hub-idp.local'],
+        'near_future:b' => [
+            'eduPersonPrincipalName' => ['NEAR_FUTURE@ssp-hub-idp.local'],
             'eduPersonTargetID' => ['22222222-2222-2222-2222-222222222222'],
-            'sn' => ['User'],
-            'givenName' => ['Warn'],
-            'mail' => ['warn@example.com'],
+            'sn' => ['Future'],
+            'givenName' => ['Near'],
+            'mail' => ['near_future@example.com'],
             'employeeNumber' => ['22222'],
-            'cn' => ['WARN'],
+            'cn' => ['NEAR_FUTURE'],
             'schacExpiryDate' => [
                 gmdate('YmdHis\Z', strtotime('+1 day')), // Very soon
             ],
         ],
-        'expired:c' => [
-            'eduPersonPrincipalName' => ['EXPIRED@ssp-hub-idp.local'],
+        'already_past:c' => [
+            'eduPersonPrincipalName' => ['ALREADY_PAST@ssp-hub-idp.local'],
             'eduPersonTargetID' => ['33333333-3333-3333-3333-333333333333'],
-            'sn' => ['User'],
-            'givenName' => ['Expired'],
-            'mail' => ['expired@example.com'],
+            'sn' => ['Past'],
+            'givenName' => ['Already'],
+            'mail' => ['already_past@example.com'],
             'employeeNumber' => ['33333'],
-            'cn' => ['EXPIRED'],
+            'cn' => ['ALREADY_PAST'],
             'schacExpiryDate' => [
                 gmdate('YmdHis\Z', strtotime('-1 day')), // In the past
             ],
