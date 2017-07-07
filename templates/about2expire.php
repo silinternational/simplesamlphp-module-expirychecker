b7a6304fef9c6d8fe8549ce68a18e1e421b61ea1
Matt Henderson <matt_henderson@sil.org>
2017-07-07 08:39:27 -0400

    Refactor about2expire files.

--- templates/about2expire.php
+++ templates/about2expire.php
@@ -1,110 +1,41 @@
 <?php
 
-/**
- * Template form for warning about password expiring soon.
- *
- * Parameters:
- * - 'formTarget': Target URL for this page. This URL will receive a POST request.
- * - 'formData': Parameters which should be included as hidden inputs.
- *
- * @package simpleSAMLphp
- * @version $Id$
- */
-
-
-//$this->data['header'] = $this->t('{expirychecker:warning:warning_header}');
-
-if ($this->data['daysleft'] == 0) {
-    
-    // Expires today.
-    
-    $this->data['header'] = $this->t('{expirychecker:warning:warning_header_today}', [
-        '%ACCOUNTNAME%' => htmlspecialchars($this->data['accountName'])
-    ]);
-    
-    $warning = $this->t('{expirychecker:warning:warning_today}', [
-        '%ACCOUNTNAME%' => htmlspecialchars($this->data['accountName'])
-    ]);
-} elseif ($this->data['daysleft'] == 1) {
-    
-    // Expires tomorrow.
-    
-    $this->data['header'] = $this->t('{expirychecker:warning:warning_header}', [
-        '%ACCOUNTNAME%' => htmlspecialchars($this->data['accountName']),
-        '%DAYS%' => $this->t('{expirychecker:warning:day}'),
-        '%DAYSLEFT%' => htmlspecialchars($this->data['daysleft']),
-    ]);
-    
-    $warning = $this->t('{expirychecker:warning:warning}', [
-        '%ACCOUNTNAME%' => htmlspecialchars($this->data['accountName']),
-        '%DAYS%' => $this->t('{expirychecker:warning:day}'),
-        '%DAYSLEFT%' => htmlspecialchars($this->data['daysleft']),
-    ]);
-} elseif ($this->data['daysleft'] < 0) {
-    
-    // Has already expired.
-    
-    $this->data['header'] = $this->t('{expirychecker:warning:warning_header_past}', [
-        '%ACCOUNTNAME%' => htmlspecialchars($this->data['accountName']),
-    ]);
-    
-    $warning = $this->t('{expirychecker:warning:warning_past}', [
-        '%ACCOUNTNAME%' => htmlspecialchars($this->data['accountName']),
-    ]);
-} else {
-    
-    // Will expire in <daysleft> days.
-    
-    $this->data['header'] = $this->t('{expirychecker:warning:warning_header}', [
-        '%ACCOUNTNAME%' => htmlspecialchars($this->data['accountName']),
-        '%DAYS%' => $this->t('{expirychecker:warning:days}'),
-        '%DAYSLEFT%' => htmlspecialchars($this->data['daysleft']),
-    ]);
-    
-    $warning = $this->t('{expirychecker:warning:warning}', [
-        '%ACCOUNTNAME%' => htmlspecialchars($this->data['accountName']),
-        '%DAYS%' => $this->t('{expirychecker:warning:days}'),
-        '%DAYSLEFT%' => htmlspecialchars($this->data['daysleft']),
-    ]);
-}
-
+$this->data['header'] = sprintf(
+    'Your password will expire in %s %s',
+    $this->data['daysleft'],
+    $this->data['dayOrDays']
+);
 $this->data['autofocus'] = 'yesbutton';
 
 $this->includeAtTemplateBase('includes/header.php');
 
 ?>
+<p>
+  The password for your <?= htmlentities($this->data['accountName']); ?>
+  account will expire on <?= htmlentities($this->data['expireOnDate']); ?>.
+</p>
+<p>
+  Would you like to update your password now?
+</p>
 
-<h3><?php echo $warning; ?></h3>
-<p><?php echo $this->t('{expirychecker:warning:expiry_date_text}') . " " . $this->data['expireOnDate']; ?></p>
-
-
-<form style="display: inline; margin: 0px; padding: 0px" action="<?php echo htmlspecialchars($this->data['formTarget']); ?>">
-
-        <?php
-                // Embed hidden fields...
-                foreach ($this->data['formData'] as $name => $value) {
-                        echo('<input type="hidden" name="' . htmlspecialchars($name) . '" value="' . htmlspecialchars($value) . '" />');
-                }
-        ?>
-
-    <input type="submit" name="changepwd" id="send" style="width:170px;" value="<?php echo htmlspecialchars($this->t('{expirychecker:warning:password_change_title}')) ?>" />
-
-</form>         
-
-<form style="display: inline; margin: 0px; padding: 0px" action="<?php echo htmlspecialchars($this->data['formTarget']); ?>">
-
-  <?php
-    // Embed hidden fields...
-    foreach ($this->data['formData'] as $name => $value) {
-      echo('<input type="hidden" name="' . htmlspecialchars($name) . '" value="' . htmlspecialchars($value) . '" />');
-    }
-  ?>
-  <input type="submit" name="continue" id="send" style="width:250px; border:0" value="<?php echo htmlspecialchars($this->t('{expirychecker:warning:btn_continue}')) ?>" />
-
+<form action="<?= htmlspecialchars($this->data['formTarget']); ?>">
+  
+    <?php foreach ($this->data['formData'] as $name => $value): ?>
+        <input type="hidden"
+               name="<?= htmlspecialchars($name); ?>"
+               value="<?= htmlspecialchars($value); ?>" />
+    <?php endforeach; ?>
+    
+    <button type="submit" id="yesbutton" name="changepwd"
+            style="padding: 4px 8px;">
+        Yes, update password
+    </button>
+    
+    <button type="submit" id="nobutton" name="continue"
+            style="padding: 4px 8px;">
+        No, continue where I was going
+    </button>
 </form>
-<br>
-<div id="trouble"></div>
-
 <?php
 
 $this->includeAtTemplateBase('includes/footer.php');
--- www/about2expire.php
+++ www/about2expire.php
@@ -1,24 +1,17 @@
 <?php
 
-SimpleSAML_Logger::info('expirychecker - User has been warned that their password will expire soon.');
-
-if ( ! array_key_exists('StateId', $_REQUEST)) {
+$stateId = filter_input(INPUT_GET, 'StateId') ?? null;
+if (empty($stateId)) {
     throw new SimpleSAML_Error_BadRequest('Missing required StateId query parameter.');
 }
 
-$id = $_REQUEST['StateId'];
-$state = SimpleSAML_Auth_State::loadState($id, 'expirychecker:about2expire');
+$state = SimpleSAML_Auth_State::loadState($stateId, 'expirychecker:about2expire');
 
 /* See if they're on their way to the change password page, and if so, let them
  * straight through.   */
-$get_data = $_GET;
-if (array_key_exists("StateId", $get_data)) {
-    $stateId = $get_data["StateId"];
-    $chgPwdUrl = "&RelayState=" .  urlencode($state['changePwdUrl']);
-    
-    if (strpos($stateId, $chgPwdUrl) !== false) {
-        SimpleSAML_Auth_ProcessingChain::resumeProcessing($state);
-    };
+$chgPwdUrlQueryParam = "&RelayState=" .  urlencode($state['changePwdUrl']);
+if (strpos($stateId, $chgPwdUrlQueryParam) !== false) {
+    SimpleSAML_Auth_ProcessingChain::resumeProcessing($state);
 }
 
 if (array_key_exists('continue', $_REQUEST)) {
@@ -34,24 +27,26 @@ if (array_key_exists('changepwd', $_REQUEST)) {
     
     // Add the original url as a parameter
     if (array_key_exists('saml:RelayState', $state)) {
-        $id = SimpleSAML_Auth_State::saveState($state, 
-              'expirychecker:about2expire');
-        $stateId = "StateId=" . $id;
+        $stateId = SimpleSAML_Auth_State::saveState(
+            $state,
+            'expirychecker:about2expire'
+        );
       
         $returnTo = sspmod_expirychecker_Utilities::getUrlFromRelayState(
-                                                    $state['saml:RelayState']);
-        if ($returnTo) {                                 
-            $changePwdUrl = $changePwdUrl . "?returnTo=" . $returnTo;
+            $state['saml:RelayState']
+        );
+        if ( ! empty($returnTo)) {                                 
+            $changePwdUrl .= '?ReturnTo=' . $returnTo;
         }
     }
 
-    $change_pwd_session = 'sent_to_change_password';
-    $session = SimpleSAML_Session::getInstance();
+    $changePwdSession = 'sent_to_change_password';
+    $session = SimpleSAML_Session::getSession();
     
     // set a value to tell us they've probably changed
     // their password, in order to allow password to get propagated
-    $session->setData('expirychecker', $change_pwd_session, 1, (60*10));
-    $session->saveSession();
+    $session->setData('expirychecker', $changePwdSession, 1, (60*10));
+    $session->save();
     SimpleSAML_Utilities::redirect($changePwdUrl, array());
 }
 
@@ -60,8 +55,11 @@ $globalConfig = SimpleSAML_Configuration::getInstance();
 
 $t = new SimpleSAML_XHTML_Template($globalConfig, 'expirychecker:about2expire.php');
 $t->data['formTarget'] = SimpleSAML_Module::getModuleURL('expirychecker/about2expire.php');
-$t->data['formData'] = array('StateId' => $id);
+$t->data['formData'] = array('StateId' => $stateId);
 $t->data['daysleft'] = $state['daysleft'];
+$t->data['dayOrDays'] = (intval($state['daysleft']) === 1 ? 'day' : 'days');
 $t->data['expireOnDate'] = $state['expireOnDate'];
 $t->data['accountName'] = $state['accountName'];
 $t->show();
+
+SimpleSAML_Logger::info('expirychecker - User has been warned that their password will expire soon.');
