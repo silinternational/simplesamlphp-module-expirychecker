d390684e359076efe2866c9fa200c9f326bd4082
stevebagwell <steve_bagwell@sil.org>
2023-01-23 11:09:45 +0000

    expiry checker is in the idp container not the behat container

--- development/run-tests.sh
+++ development/run-tests.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
-
-# Try to install composer dev dependencies
-cd /data/vendor/simplesamlphp/simplesamlphp/modules/expirychecker
-
 # If the following fails, exit.
 set -e
-COMPOSER_ROOT_VERSION=dev-develop composer install --no-interaction --no-scripts
-
-cd /data/
 
 # Run the feature tests
 ./vendor/bin/behat --config=features/behat.yml
-
-# If they failed, exit.
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
