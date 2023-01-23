e7720cdc2c66b52c11f0978fb782dffa40f744b4
stevebagwell <steve_bagwell@sil.org>
2023-01-23 11:22:55 +0000

    ensure composer installs behat for tests

--- development/run-tests.sh
+++ development/run-tests.sh
@@ -3,5 +3,9 @@
 # If the following fails, exit.
 set -e
 
+COMPOSER_ROOT_VERSION=dev-develop composer install --no-interaction --no-scripts
+
+cd /data/
+
 # Run the feature tests
 ./vendor/bin/behat --config=features/behat.yml
